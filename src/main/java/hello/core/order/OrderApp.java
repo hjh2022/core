13f47bb3100cb0b01a3e6e258da2c2b602c41cdf
jinhwanhan <jh2022101@gmail.com>
2023-01-10 10:00:48 +0900

    1/10 싱글톤 주의점

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -4,13 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public  static void main(String[] args){
 
-            AppConfig appConfig = new AppConfig();
-            MemberService memberService = appConfig.memberService();//AppConfig 를 통해서..
-            OrderService orderService = appConfig.orderService();//AppConfig 를 통해서....
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+
+//            AppConfig appConfig = new AppConfig();
+//            MemberService memberService = appConfig.memberService();//AppConfig 를 통해서..
+//            OrderService orderService = appConfig.orderService();//AppConfig 를 통해서....
 
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,31 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+    }
+
+    //ThreadA: A사용자 10000원 주문
+
+
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
